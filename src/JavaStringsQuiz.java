5937eadd9c3452a46cc002e86ec286bc58814b2a
Anurag Singh <anurag.singh2_cs20@gla.ac.in>
2023-03-28 14:47:50 +0530

    INITIAL

---
+++ src/JavaStringsQuiz.java
@@ -0,0 +1,46 @@
+public class JavaStringsQuiz {
+    public static void main(String[] args) {
+        String name="JavaConceptOfTheDay".substring(4);
+//        System.out.println(name);
+//        System.out.println("1".concat("2").repeat(5));
+//        System.out.println("1".concat("2").repeat(5).charAt(7));
+//        String string="String".replace('i','0');
+//        System.out.println(string.substring(2,5));
+//        System.out.println("Java"==new String("Java"));
+//        String str=" Java\tConcept\tOf\tThe\tDay  ".strip();
+//        System.out.println(str);
+//        if("string".toUpperCase()=="STRING"){
+//            System.out.println(true);
+//        }
+//        else {
+//            System.out.println(false);
+//        }
+//        TO COMPARE STRING IN JAVA
+//        String str1="java";
+//        String str2="java";
+//        System.out.println(str1==str2);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.hashCode()==str2.hashCode());
+//        String str1="java";
+//        String str2=new String("java");
+//        System.out.println(str1==str2);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.hashCode()==str2.hashCode());
+//        String str1=new String("java");
+//        String str2=new String("java");
+//        System.out.println(str1==str2);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.hashCode()==str2.hashCode());
+//        String str1="java";
+//        String str2=str1.intern();
+//        System.out.println(str1==str2);
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.hashCode()==str2.hashCode());
+        String str1="java";
+        String str2=str1.intern();
+        String str3=new String("java");
+        System.out.println(str1==str2);
+        System.out.println(str2==str3);
+        System.out.println(str3==str1);
+    }
+}
