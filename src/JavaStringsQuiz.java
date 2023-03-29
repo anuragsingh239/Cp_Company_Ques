cd0b506ff8d0e8658beeb62d4705287955720b1e
Anurag Singh <anurag.singh2_cs20@gla.ac.in>
2023-03-29 14:48:07 +0530

    INITIAL

--- src/JavaStringsQuiz.java
+++ src/JavaStringsQuiz.java
@@ -1,6 +1,6 @@
 public class JavaStringsQuiz {
     public static void main(String[] args) {
-        String name="JavaConceptOfTheDay".substring(4);
+//        String name="JavaConceptOfTheDay".substring(4);
 //        System.out.println(name);
 //        System.out.println("1".concat("2").repeat(5));
 //        System.out.println("1".concat("2").repeat(5).charAt(7));
@@ -42,8 +42,34 @@ public class JavaStringsQuiz {
 //        System.out.println(str1==str2);
 //        System.out.println(str2==str3);
 //        System.out.println(str3==str1);
-        String s1="1";
-        System.out.println(s1.concat("2").concat("3"));
-        System.out.println("java"+1000+2000+3000);
+//        String s1="1";
+//        System.out.println(s1.concat("2").concat("3"));
+//        System.out.println("java"+1000+2000+3000);
+//        String str1=new String("java");
+//        String str2=new String("java");
+//        System.out.println(str1==str2);
+//        System.out.println(str1==str2.intern());
+//        System.out.println(str1.intern()==str2.intern());
+//        System.out.println("0".indent(0));
+//        System.out.println("1".indent(1));
+//        System.out.println("2".indent(2));
+//        System.out.println("3".indent(3));
+//        System.out.println("4".indent(4));System.out.println("5".indent(5));
+//    System.out.println("A".compareTo("B"));
+//    System.out.println("B".compareTo("A"));
+//    String string="JAVA";
+//    StringBuffer sbuffer=new StringBuffer(string);
+//    StringBuilder sbuilder=new StringBuilder(string);
+//        System.out.println(string.equals(sbuffer));
+//        System.out.println(string.equals(sbuilder));
+//        System.out.println(sbuffer.equals(sbuilder));
+//        "ABC123abc".chars().forEach(System.out::println);
+//        String s1="null"+null+1;
+//        System.out.println(s1);
+//        String s2=1+null+"null";Shows error
+//        System.out.println(s1);
+        String str="Java Concept Of The Day";
+        System.out.println(str.indexOf('a')+str.indexOf("Day"));
+        System.out.println(str.indexOf('a',5));
     }
 }
